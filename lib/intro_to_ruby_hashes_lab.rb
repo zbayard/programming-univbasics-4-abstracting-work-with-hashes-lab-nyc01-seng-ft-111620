47424bc0d3ca4f4865fd8a13b13d0ba5e33a7598
Zach Bayard <zbayard@gmail.com>
2020-10-26 16:23:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,6 @@
 def my_hash_creator(key, value)
-  person = {
-    key: "Sam" ,
-    value: 20
+  {
+    key => value
   }
   # return a hash that includes the key and value parameters passed into this method
 end
